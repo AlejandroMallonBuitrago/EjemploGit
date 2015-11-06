42c2b773775d46c48292b9e07ade6dee19f16fc0
AlejandroMallonBuitrago <alejandromallonbuitrago@gmail.com>
2015-11-06 17:12:44 +0100

    Principio de proyecto
    sale un texto que pone hola git

---
+++ src/Ejercicio1.java
@@ -0,0 +1,14 @@
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+
+public class Ejercicio1 extends GraphicsProgram{
+
+	
+	public void run(){
+		
+		GLabel etiqueta = new GLabel("HOLA GIT");
+		
+		add(etiqueta,200,200);
+		
+	}
+}
